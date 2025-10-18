be71585f63a82821777cdc8ab12b34459a8b58d3
LAB_E_REAL <labe@continental.edu.pe>
2025-10-17 20:56:38 -0500

    python_pusher

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=["POST"])
+def hola_mundo():
+    import pusher
+    data = request.get_json()
+
+    pusher_client = pusher.Pusher(
+        app_id = "2062326",
+        key = "8991fbde10313380964c",
+        secret = "57b5781a85767841a9c8",
+        cluster = "ap3",
+        ssl= True
+    )
+
+    pusher_client.trigger('my-channel','my-event', {'message':data["message"]})
+    return ".."
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
